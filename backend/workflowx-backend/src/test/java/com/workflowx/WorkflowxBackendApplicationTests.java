fe96a53d16bbe5da4e9f9765ff036bb49842abb2
Dev Sharma <devsh8126@gmail.com>
2026-06-19 15:36:36 +0530

    Initial project structure

---
+++ backend/workflowx-backend/src/test/java/com/workflowx/WorkflowxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workflowx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
